7d7896272bebb2d01d62c9b97ed3f87d0d562939
AlexandreRouma <alexandre.rouma@gmail.com>
2022-07-02 00:25:58 +0200

    Added digital demod blocks

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,37 @@
 #include <stdio.h>
 #include <dsp/bench/speed_tester.h>
-#include <dsp/multirate/polyphase_bank.h>
-#include <dsp/window/nuttall.h>
-#include <dsp/taps/windowed_sinc.h>
-#include <dsp/window/blackman.h>
-#include <dsp/window/blackman_harris.h>
-#include <dsp/window/blackman_nuttall.h>
-#include <dsp/window/hamming.h>
-#include <dsp/window/hann.h>
-#include <dsp/window/nuttall.h>
+#include <dsp/clock_recovery/mm.h>
 
 #define TEST_BUFFER_SIZE    (2400000 / 200)
 #define TEST_DURATION       1000.0
 #define TEST_COUNT          5
 
 #define IN_TYPE             dsp::complex_t
-#define OUT_TYPE            dsp::stereo_t
+#define OUT_TYPE            dsp::complex_t
 
 int main() {
-    int phases = 128;
-    double bw = 0.5 / (double)phases;
-    dsp::tap<float> lp = dsp::taps::windowedSinc<float>(phases * 8, dsp::math::freqToOmega(bw, 1.0), dsp::window::nuttall, 128.0);
-    dsp::multirate::PolyphaseBank bank = dsp::multirate::buildPolyphaseBank<float>(phases, lp);
-    for (int i = 0; i < phases; i++) {
-        printf("%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n", bank.phases[i][0], bank.phases[i][1], bank.phases[i][2], bank.phases[i][3], bank.phases[i][4], bank.phases[i][5], bank.phases[i][6], bank.phases[i][7]);
-    }
+    dsp::stream<IN_TYPE>* input;
+    dsp::stream<OUT_TYPE>* output;
 
+    // ============= DSP Under Test =============
+    input = new dsp::stream<IN_TYPE>;
 
-    // dsp::stream<IN_TYPE>* input;
-    // dsp::stream<OUT_TYPE>* output;
+    dsp::clock_recovery::MM<dsp::complex_t> dut(input, 2.0, 0.000001, 0.02, 0.01);
 
-    // // ============= DSP Under Test =============
-    // input = new dsp::stream<IN_TYPE>;
-
-    // dsp::demod::AM<OUT_TYPE> dut(input, dsp::demod::AM<OUT_TYPE>::AGCMode::CARRIER, 10000.0, 0.001, 0.001, 0.001, 30000.0);
-
-    // output = &dut.out;
-    // // ==========================================
+    output = &dut.out;
+    // ==========================================
 
     // // dut.setInSamplerate(2400000.0);
     // // dut.setOutSamplerate(24000.0);
 
-    // // Run benchmark
-    // dsp::bench::SpeedTester<IN_TYPE, OUT_TYPE> st(input, output);
-    // dut.start();
-    // for (int i = 0; i < TEST_COUNT; i++) {
-    //     dut.reset();
-    //     printf("%lf MS/s\n", st.benchmark(TEST_DURATION, TEST_BUFFER_SIZE) / 1000000.0);
-    // }
-    // dut.stop();
+    // Run benchmark
+    dsp::bench::SpeedTester<IN_TYPE, OUT_TYPE> st(input, output);
+    dut.start();
+    for (int i = 0; i < TEST_COUNT; i++) {
+        dut.reset();
+        printf("%lf MS/s\n", st.benchmark(TEST_DURATION, TEST_BUFFER_SIZE) / 1000000.0);
+    }
+    dut.stop();
 
     // for (double f = -0.5; f <= 0.5; f += 0.001) {
     //     // Generate signal
